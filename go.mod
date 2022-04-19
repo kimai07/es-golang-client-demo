f80570d05707ba49cbdb0386d806b09463bf2edf
kenji imai <imakenfever@gmail.com>
2022-04-19 20:02:20 +0900

    for es verup 8.1

--- go.mod
+++ go.mod
@@ -1,5 +1,7 @@
 module github.com/kenji-imi/es-golang-client-demo
 
-go 1.14
+go 1.18
 
-require github.com/elastic/go-elasticsearch v0.0.0
+require github.com/elastic/go-elasticsearch/v8 v8.1.0
+
+require github.com/elastic/elastic-transport-go/v8 v8.1.0 // indirect
--- main.go
+++ main.go
@@ -8,8 +8,8 @@ import (
 	"strings"
 	"sync"
 
-	"github.com/elastic/go-elasticsearch"
-	"github.com/elastic/go-elasticsearch/esapi"
+	elasticsearch "github.com/elastic/go-elasticsearch/v8"
+	"github.com/elastic/go-elasticsearch/v8/esapi"
 )
 
 func newClientWithConfig() *elasticsearch.Client {
